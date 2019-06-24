2c22e6527fb5b8b1dc0fdc4b0172b34436a0d18b
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-24 17:30:41 +0200

    feat(Startup.cs): add ApplicationContext

---
+++ hotel-booking-app/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace hotel_booking_app
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+    }
+}
