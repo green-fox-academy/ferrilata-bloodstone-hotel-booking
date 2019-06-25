495552fef8d2af6fc8d9ef7e7b8c819fe5899412
Gergely Kocsis <koger23@gmail.com>
2019-06-25 13:57:23 +0200

    feat(UserServiceTest): create static method for context options

--- hotel-booking-app-tests/Services/UserServiceTest.cs
+++ hotel-booking-app-tests/Services/UserServiceTest.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelBookingApp;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace hotel_booking_app_tests.Services
 {
     [TestClass]
     class UserServiceTest
     {
-    }
-}
+        public static DbContextOptions<ApplicationContext> GetTestDbOptions()
+        {
+            return new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase(databaseName: "hotel-booking-testdb")
+                .Options;
+        }
