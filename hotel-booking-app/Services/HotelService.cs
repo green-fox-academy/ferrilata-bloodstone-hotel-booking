ee14f18cb9e3fbfe3d485a39fe43089c4ddb93d8
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-25 17:13:39 +0200

    feat(HotelService): add method with pagination

--- hotel-booking-app/Services/HotelService.cs
+++ hotel-booking-app/Services/HotelService.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelBookingApp.Models;
 
 namespace HotelBookingApp.Services
 {
-    public class HotelService
+    public class HotelService : IHotelService
     {
+        public IEnumerable<HotelModel> FindAllPaginated(int currentPage = 1)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
