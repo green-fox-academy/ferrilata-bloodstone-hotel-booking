0a5ef064a59bf75b472eddeb6e9bc68607a98ecb
Gergely Kocsis <koger23@gmail.com>
2019-06-25 12:17:39 +0200

    fix(IUserService): fix return type of GetById()

--- hotel-booking-app/Services/IUserService.cs
+++ hotel-booking-app/Services/IUserService.cs
@@ -1,12 +1,13 @@
 ﻿using HotelBookingApp.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HotelBookingApp.Services
 {
     public interface IUserService
     {
         IEnumerable<UserModel> GetAll();
-        UserModel GetById(long id);
+        Task<UserModel> GetById(long id);
         UserModel Create(UserModel user);
         void Update(UserModel userParam);
         void Delete(long id);
