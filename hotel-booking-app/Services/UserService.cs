073f854da3d69b17493c5d77b4d943b28fb5d8cd
Gergely Kocsis <koger23@gmail.com>
2019-06-25 11:40:06 +0200

    feat(UserService): init service class with implemented interface

---
+++ hotel-booking-app/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using HotelBookingApp.Models;
+
+namespace HotelBookingApp.Services
+{
+    public class UserService : IUserService
+    {
+        public UserModel Create(UserModel user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public UserModel GetById(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(UserModel userParam)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
