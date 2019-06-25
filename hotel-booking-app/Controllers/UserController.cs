b5df03ae29d1044d6d77329514163cbb9110a9e9
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-25 11:48:51 +0200

    feat(UserController): add UserService

--- hotel-booking-app/Controllers/UserController.cs
+++ hotel-booking-app/Controllers/UserController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelBookingApp.Services;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace HotelBookingApp.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
         [HttpPost]
         public async Task<IActionResult> Login()
         {
+
             return View();
         }
 
