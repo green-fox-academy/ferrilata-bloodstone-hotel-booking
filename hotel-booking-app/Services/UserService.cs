031fe644dacc88070b4706108f772c04c1884e83
Gergely Kocsis <koger23@gmail.com>
2019-06-25 12:16:44 +0200

    feat(UserService): add contstructor

--- hotel-booking-app/Services/UserService.cs
+++ hotel-booking-app/Services/UserService.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.Generic;
 using HotelBookingApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HotelBookingApp.Services
 {
     public class UserService : IUserService
     {
+        private readonly ApplicationContext applicationContext;
+
+        public UserService(ApplicationContext context)
+        {
+            this.applicationContext = context;
+        }
+
         public UserModel Create(UserModel user)
         {
             throw new System.NotImplementedException();
