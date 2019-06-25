8b67a8ac69b8f2ea32cf91171830daf8b83d302b
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-25 11:43:32 +0200

    feat(UserController): init

---
+++ hotel-booking-app/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HotelBookingApp.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
