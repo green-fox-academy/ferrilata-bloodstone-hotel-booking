3f059e8abc025d4441c37c0881dee3d2f8619535
Gergely Kocsis <koger23@gmail.com>
2019-06-25 11:37:35 +0200

    feat(IUserService): add methods

--- hotel-booking-app/Services/IUserService.cs
+++ hotel-booking-app/Services/IUserService.cs
@@ -1,6 +1,14 @@
-﻿namespace HotelBookingApp.Services
+﻿using HotelBookingApp.Models;
+using System.Collections.Generic;
+
+namespace HotelBookingApp.Services
 {
     public interface IUserService
     {
+        IEnumerable<UserModel> GetAll();
+        UserModel GetById(long id);
+        UserModel Create(UserModel user);
+        void Update(UserModel userParam);
+        void Delete(long id);
     }
 }
