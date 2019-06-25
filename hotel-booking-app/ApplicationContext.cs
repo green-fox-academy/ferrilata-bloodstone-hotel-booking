7cc8533883d117d77191e0cbaa390727d69b82f8
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-06-25 10:57:44 +0200

    feat(applicationContext): add dataseed

--- hotel-booking-app/ApplicationContext.cs
+++ hotel-booking-app/ApplicationContext.cs
@@ -9,6 +9,13 @@ namespace HotelBookingApp
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
         }
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HotelModel>().HasData(
+                new HotelModel() { Id = 1, Name = "hotel1", Description = "description1", Price = 1 },
+                new HotelModel() { Id = 2, Name = "hotel2", Description = "description2", Price = 2 },
+                new HotelModel() { Id = 3, Name = "hotel3", Description = "description3", Price = 3 }
+                ) ;
+        }
     }
 }
--- hotel-booking-app/Models/HotelModel.cs
+++ hotel-booking-app/Models/HotelModel.cs
@@ -11,5 +11,17 @@ namespace HotelBookingApp.Models
         public string  Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
+
+        public HotelModel(long id, string name, string description, int price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public HotelModel()
+        {
+        }
     }
 }
