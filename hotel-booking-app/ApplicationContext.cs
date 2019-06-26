0d5015bf0591677c9b71537ebd277f38eb1553fb
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-26 09:51:45 +0200

    style(ApplicationContext): remove indentation

--- hotel-booking-app/ApplicationContext.cs
+++ hotel-booking-app/ApplicationContext.cs
@@ -16,7 +16,7 @@ namespace HotelBookingApp
                 new HotelModel() { Id = 1, Name = "hotel1", Description = "description1", Price = 1 },
                 new HotelModel() { Id = 2, Name = "hotel2", Description = "description2", Price = 2 },
                 new HotelModel() { Id = 3, Name = "hotel3", Description = "description3", Price = 3 }
-                ) ;
+            );
         }
     }
 }
