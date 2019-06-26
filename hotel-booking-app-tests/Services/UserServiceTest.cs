7752d83a8d86a3596aac1520b9e5c441471257a6
Gergely Kocsis <koger23@gmail.com>
2019-06-26 15:47:29 +0200

    refactor(GetDbSetForAsynctest, UserServiceTest): create seperate class
    Create generic static method and internal class moved to GetDbSetForAsynctest().

--- hotel-booking-app-tests/Services/UserServiceTest.cs
+++ hotel-booking-app-tests/Services/UserServiceTest.cs
@@ -6,10 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -17,8 +14,6 @@ namespace hotel_booking_app_tests.Services
 {
     public class UserServiceTest
     {
-
-
         [Fact]
         public async Task Create_WithNewUser_ResultsOk()
         {
@@ -100,23 +95,8 @@ namespace hotel_booking_app_tests.Services
         {
             // Arrange
             var user = new UserModel { Id = 1, Email = "testUser@example.com", Password = "12344321" };
-            var data = new List<UserModel>
-            {
-                user
-            }.AsQueryable();
-            var mockSet = new Mock<DbSet<UserModel>>();
-            mockSet.As<IDbAsyncEnumerable<UserModel>>()
-                .Setup(m => m.GetAsyncEnumerator())
-                .Returns(new TestDbAsyncEnumerator<UserModel>(data.GetEnumerator()));
-
-            mockSet.As<IQueryable<UserModel>>()
-                .Setup(m => m.Provider)
-                .Returns(new TestDbAsyncQueryProvider<UserModel>(data.Provider));
-
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
+            var userQueryableList = new List<UserModel> { user };
+            var mockSet = GetDbSetForAsynctest.GetMockDbSetForAsyncMethodTest<UserModel>(userQueryableList);
             var mockContext = new Mock<ApplicationContext>(ContextMockingHelpers.GetTestDbOptions());
             mockContext.Setup(c => c.Users).Returns(mockSet.Object);
             var userService = new UserService(mockContext.Object);
@@ -137,23 +117,8 @@ namespace hotel_booking_app_tests.Services
         {
             // Arrange
             var user = new UserModel { Id = 1, Email = "testUser@example.com", Password = "12344321" };
-            var data = new List<UserModel>
-            {
-                user
-            }.AsQueryable();
-            var mockSet = new Mock<DbSet<UserModel>>();
-            mockSet.As<IDbAsyncEnumerable<UserModel>>()
-                .Setup(m => m.GetAsyncEnumerator())
-                .Returns(new TestDbAsyncEnumerator<UserModel>(data.GetEnumerator()));
-
-            mockSet.As<IQueryable<UserModel>>()
-                .Setup(m => m.Provider)
-                .Returns(new TestDbAsyncQueryProvider<UserModel>(data.Provider));
-
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
+            var userQueryableList = new List<UserModel> { user };
+            var mockSet = GetDbSetForAsynctest.GetMockDbSetForAsyncMethodTest<UserModel>(userQueryableList);
             var mockContext = new Mock<ApplicationContext>(ContextMockingHelpers.GetTestDbOptions());
             mockContext.Setup(c => c.Users).Returns(mockSet.Object);
             mockContext.Setup(c => c.Users.FindAsync(It.IsAny<long>())).ReturnsAsync(user);
@@ -171,22 +136,8 @@ namespace hotel_booking_app_tests.Services
         {
             // Arrange
             var user = new UserModel { Id = 1, Email = "testUser@example.com", Password = "12344321" };
-            var data = new List<UserModel>
-            {
-                user
-            }.AsQueryable();
-            var mockSet = new Mock<DbSet<UserModel>>();
-            mockSet.As<IDbAsyncEnumerable<UserModel>>()
-                .Setup(m => m.GetAsyncEnumerator())
-                .Returns(new TestDbAsyncEnumerator<UserModel>(data.GetEnumerator()));
-
-            mockSet.As<IQueryable<UserModel>>()
-                .Setup(m => m.Provider)
-                .Returns(new TestDbAsyncQueryProvider<UserModel>(data.Provider));
-
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            var userQueryableList = new List<UserModel> { user };
+            var mockSet = GetDbSetForAsynctest.GetMockDbSetForAsyncMethodTest<UserModel>(userQueryableList);
 
             var mockContext = new Mock<ApplicationContext>(ContextMockingHelpers.GetTestDbOptions());
             mockContext.Setup(c => c.Users).Returns(mockSet.Object);
@@ -203,22 +154,8 @@ namespace hotel_booking_app_tests.Services
         {
             // Arrange
             var user = new UserModel { Id = 1, Email = "testUser@example.com", Password = "12344321" };
-            var data = new List<UserModel>
-            {
-                user
-            }.AsQueryable();
-            var mockSet = new Mock<DbSet<UserModel>>();
-            mockSet.As<IDbAsyncEnumerable<UserModel>>()
-                .Setup(m => m.GetAsyncEnumerator())
-                .Returns(new TestDbAsyncEnumerator<UserModel>(data.GetEnumerator()));
-
-            mockSet.As<IQueryable<UserModel>>()
-                .Setup(m => m.Provider)
-                .Returns(new TestDbAsyncQueryProvider<UserModel>(data.Provider));
-
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<UserModel>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            var userQueryableList = new List<UserModel> { user };
+            var mockSet = GetDbSetForAsynctest.GetMockDbSetForAsyncMethodTest<UserModel>(userQueryableList);
 
             var mockContext = new Mock<ApplicationContext>(ContextMockingHelpers.GetTestDbOptions());
             mockContext.Setup(c => c.Users).Returns(mockSet.Object);
@@ -230,101 +167,6 @@ namespace hotel_booking_app_tests.Services
 
             // Assert
             mockSet.Verify(m => m.Update(It.IsAny<UserModel>()), Times.Once(), "User updated more or less than 1.");
-        }
-    }
-    internal class TestDbAsyncQueryProvider<TEntity> : IDbAsyncQueryProvider
-    {
-        private readonly IQueryProvider _inner;
-
-        internal TestDbAsyncQueryProvider(IQueryProvider inner)
-        {
-            _inner = inner;
-        }
-
-        public IQueryable CreateQuery(Expression expression)
-        {
-            return new TestDbAsyncEnumerable<TEntity>(expression);
-        }
-
-        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
-        {
-            return new TestDbAsyncEnumerable<TElement>(expression);
-        }
-
-        public object Execute(Expression expression)
-        {
-            return _inner.Execute(expression);
-        }
-
-        public TResult Execute<TResult>(Expression expression)
-        {
-            return _inner.Execute<TResult>(expression);
-        }
-
-        public Task<object> ExecuteAsync(Expression expression, CancellationToken cancellationToken)
-        {
-            return Task.FromResult(Execute(expression));
-        }
-
-        public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
-        {
-            return Task.FromResult(Execute<TResult>(expression));
-        }
-    }
-
-    internal class TestDbAsyncEnumerable<T> : EnumerableQuery<T>, IDbAsyncEnumerable<T>, IQueryable<T>
-    {
-        public TestDbAsyncEnumerable(IEnumerable<T> enumerable)
-            : base(enumerable)
-        { }
-
-        public TestDbAsyncEnumerable(Expression expression)
-            : base(expression)
-        { }
-
-        public IDbAsyncEnumerator<T> GetAsyncEnumerator()
-        {
-            return new TestDbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
-        }
-
-        IDbAsyncEnumerator IDbAsyncEnumerable.GetAsyncEnumerator()
-        {
-            return GetAsyncEnumerator();
-        }
-
-        IQueryProvider IQueryable.Provider
-        {
-            get { return new TestDbAsyncQueryProvider<T>(this); }
-        }
-    }
-
-    internal class TestDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
-    {
-        private readonly IEnumerator<T> _inner;
-
-        public TestDbAsyncEnumerator(IEnumerator<T> inner)
-        {
-            _inner = inner;
-        }
-
-        public void Dispose()
-        {
-            _inner.Dispose();
-        }
-
-        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
-        {
-            return Task.FromResult(_inner.MoveNext());
-        }
-
-        public T Current
-        {
-            get { return _inner.Current; }
-        }
-
-        object IDbAsyncEnumerator.Current
-        {
-            get { return Current; }
         }
     }
 }
