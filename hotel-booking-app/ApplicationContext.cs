e5dade981284ad8cb13b38455bdac0ac1b65f734
Gergely Kocsis <koger23@gmail.com>
2019-06-26 11:23:54 +0200

    fix(ApplicationContext): set DbSets to virtual
    Needed to make them mockable.

--- hotel-booking-app/ApplicationContext.cs
+++ hotel-booking-app/ApplicationContext.cs
@@ -4,8 +4,8 @@ namespace HotelBookingApp
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<HotelModel> Hotels { get; set; }
-        public DbSet<UserModel> Users { get; set; }
+        public virtual DbSet<HotelModel> Hotels { get; set; }
+        public virtual DbSet<UserModel> Users { get; set; }
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
         }
