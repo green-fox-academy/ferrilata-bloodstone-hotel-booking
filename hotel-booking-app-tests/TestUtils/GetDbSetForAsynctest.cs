4286505e5ed62b0efa8a894b239face624a23ebd
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-28 15:30:33 +0200

    feat(PaginatedListTests): init

--- hotel-booking-app-tests/TestUtils/GetDbSetForAsynctest.cs
+++ hotel-booking-app-tests/TestUtils/GetDbSetForAsynctest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using Moq;
 using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
@@ -11,7 +12,7 @@ namespace HotelBookingAppTests.TestUtils
 {
     class GetDbSetForAsynctest
     {
-        public static Mock<DbSet<T>> GetMockDbSetForAsyncMethodTest<T>(List<T> dataList) where T : class
+        public static Mock<DbSet<T>> GetMockDbSetForAsyncMethodTest<T>(IEnumerable<T> dataList) where T : class
         {
             var data = dataList.AsQueryable(); 
             var mockSet = new Mock<DbSet<T>>();
@@ -26,6 +27,24 @@ namespace HotelBookingAppTests.TestUtils
             mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
             mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
             mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            return mockSet;
+        }
+
+        public static Mock<IQueryable<T>> GetQueriable<T>(IQueryable<T> source) where T : class
+        {
+            var mockSet = new Mock<IQueryable<T>>();
+
+            mockSet.As<IAsyncEnumerable<T>>()
+                .Setup(m => m.GetEnumerator())
+                .Returns(new TestAsyncEnumerator<T>(source.GetEnumerator()));
+
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.Provider)
+                .Returns(new TestAsyncQueryProvider<T>(source.Provider));
+
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(source.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(source.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => source.GetEnumerator());
             return mockSet;
         }
     }
@@ -123,6 +142,95 @@ namespace HotelBookingAppTests.TestUtils
         object IDbAsyncEnumerator.Current
         {
             get { return Current; }
+        }
+    }
+
+    internal class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+    {
+        private readonly IQueryProvider _inner;
+
+        internal TestAsyncQueryProvider(IQueryProvider inner)
+        {
+            _inner = inner;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new TestAsyncEnumerable<TEntity>(expression);
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new TestAsyncEnumerable<TElement>(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return _inner.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _inner.Execute<TResult>(expression);
+        }
+
+        public IAsyncEnumerable<TResult> ExecuteAsync<TResult>(Expression expression)
+        {
+            return new TestAsyncEnumerable<TResult>(expression);
+        }
+
+        public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute<TResult>(expression));
+        }
+    }
+
+    internal class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public TestAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public TestAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        public IAsyncEnumerator<T> GetEnumerator()
+        {
+            return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new TestAsyncQueryProvider<T>(this); }
+        }
+    }
+
+    internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _inner;
+
+        public TestAsyncEnumerator(IEnumerator<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public void Dispose()
+        {
+            _inner.Dispose();
+        }
+
+        public T Current
+        {
+            get
+            {
+                return _inner.Current;
+            }
+        }
+
+        public Task<bool> MoveNext(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_inner.MoveNext());
         }
     }
 }
---
+++ hotel-booking-app-tests/Utils/PaginatedListTests.cs
@@ -0,0 +1,49 @@
+﻿using HotelBookingApp.Utils;
+using HotelBookingAppTests.TestUtils;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HotelBookingAppTests.Utils
+{
+    public class PaginatedListTests
+    {
+        private IQueryable<Model> source;
+
+        [Fact]
+        public async Task PaginatedList_WhenCreating_PropertiesShouldBeOk()
+        {
+            source = InitSource();
+
+            var mockSet = GetDbSetForAsynctest.GetQueriable(source);
+
+            var paginatedList = await PaginatedList<Model>.CreateAsync(mockSet.Object, 1, 1);
+
+            Assert.Equal(1, paginatedList.CurrentPage);
+        }
+
+        private IQueryable<Model> InitSource()
+        {
+            return new List<Model> {
+                new Model { Name = "Krisz" },
+                new Model { Name = "Geri" },
+                new Model { Name = "Orsi" },
+                new Model { Name = "Kata" },
+                new Model { Name = "Alex" },
+                new Model { Name = "Attila" },
+                new Model { Name = "Andras" },
+                new Model { Name = "Dani" },
+                new Model { Name = "Zsolt" },
+                new Model { Name = "Soma" },
+                new Model { Name = "Roza" },
+                new Model { Name = "Anna" },
+            }.AsQueryable();
+        }
+
+        public class Model
+        {
+            public string Name { get; set; }
+        }
+    }
+}
