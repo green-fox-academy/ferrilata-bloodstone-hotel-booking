27c67d6bae7bd9dc4181bfba83bc5382425db336
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-07-01 15:09:23 +0200

    add(Models): add models Bed, Room, Location, PropertyType update Hotel- no relations

---
+++ hotel-booking-app/Models/Hotel/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelBookingApp.Models.Hotel
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+
+    }
+}
