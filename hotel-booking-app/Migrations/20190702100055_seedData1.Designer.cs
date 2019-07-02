665277a87b8e44b3a7da037b45d18eeb50ca4270
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-07-02 12:07:23 +0200

    feat(ApplicationContext): ad dataSeed for relational tables

--- hotel-booking-app/Migrations/20190702100055_seedData1.Designer.cs
+++ hotel-booking-app/Migrations/20190702100055_seedData1.Designer.cs
@@ -4,14 +4,16 @@ using HotelBookingApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelBookingApp.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190702100055_seedData1")]
+    partial class seedData1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
