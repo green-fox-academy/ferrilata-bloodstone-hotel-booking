fd8d5e99a7c93165e0b518ad63ca92d37f5547be
Orsolya Jakabova <orsolya.jakabova@gmail.com>
2019-07-03 09:55:38 +0200

    add(Program.cs): add config in program.cs fix datainitializer class, separate seeding methods

--- hotel-booking-app/ApplicationContext.cs
+++ hotel-booking-app/ApplicationContext.cs
@@ -34,24 +34,24 @@ namespace HotelBookingApp
                 .WithMany(t => t.RoomBeds)
                 .HasForeignKey(pt => pt.BedId);
 
-            modelBuilder.Entity<PropertyType>().HasData(
-                new PropertyType { PropertyTypeId = 1, Type = "Apartment" },
-                new PropertyType { PropertyTypeId = 2, Type = "Hostel" },
-                new PropertyType { PropertyTypeId = 3, Type = "Hotel" },
-                new PropertyType { PropertyTypeId = 4, Type = "Guesthouse" }
-                );
+            //modelBuilder.Entity<PropertyType>().HasData(
+            //    new PropertyType { PropertyTypeId = 1, Type = "Apartment" },
+            //    new PropertyType { PropertyTypeId = 2, Type = "Hostel" },
+            //    new PropertyType { PropertyTypeId = 3, Type = "Hotel" },
+            //    new PropertyType { PropertyTypeId = 4, Type = "Guesthouse" }
+            //    );
 
-            modelBuilder.Entity<Location>().HasData(
-                new Location { LocationId = 1, Country = "Moon", City = "Moon", Region = "Moon District?", Address = "a street 1" },
-                new Location { LocationId = 2, Country = "Mars", City = "Mars", Region = "Mars District?", Address = "a street mars" },
-                new Location { LocationId = 3, Country = "Pluto", City = "p", Region = "Pluto District?", Address = "a street p" }
-                );
+            //modelBuilder.Entity<Location>().HasData(
+            //    new Location { LocationId = 1, Country = "Moon", City = "Moon", Region = "Moon District?", Address = "a street 1" },
+            //    new Location { LocationId = 2, Country = "Mars", City = "Mars", Region = "Mars District?", Address = "a street mars" },
+            //    new Location { LocationId = 3, Country = "Pluto", City = "p", Region = "Pluto District?", Address = "a street p" }
+            //    );
 
-            modelBuilder.Entity<Hotel>().HasData(
-                new Hotel() { HotelId = 1, Name = "hotel1", Description = "description1", Price = 1, StarRating = 1, LocationId = 1, PropertyTypeId = 1 },
-                new Hotel() { HotelId = 2, Name = "hotel2", Description = "description2", Price = 2, StarRating = 1, LocationId = 2, PropertyTypeId = 2 },
-                new Hotel() { HotelId = 3, Name = "hotel3", Description = "description3", Price = 3, StarRating = 1, LocationId = 3, PropertyTypeId = 1 }
-            );
+            //modelBuilder.Entity<Hotel>().HasData(
+            //    new Hotel() { HotelId = 1, Name = "hotel1", Description = "description1", Price = 1, StarRating = 1, LocationId = 1, PropertyTypeId = 1 },
+            //    new Hotel() { HotelId = 2, Name = "hotel2", Description = "description2", Price = 2, StarRating = 1, LocationId = 2, PropertyTypeId = 2 },
+            //    new Hotel() { HotelId = 3, Name = "hotel3", Description = "description3", Price = 3, StarRating = 1, LocationId = 3, PropertyTypeId = 1 }
+            //);
         }
     }
 }
