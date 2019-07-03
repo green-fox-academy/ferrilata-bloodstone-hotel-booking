7dd7b82331e22b5c5e01c2af6d8780d7a462384a
Gergely Kocsis <koger23@gmail.com>
2019-07-03 14:51:19 +0200

    feat(IImageService): add methods

--- hotel-booking-app/Services/IImageService.cs
+++ hotel-booking-app/Services/IImageService.cs
@@ -1,6 +1,17 @@
-﻿namespace HotelBookingApp.Services
+﻿using HotelBookingApp.Models.Image;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HotelBookingApp.Services
 {
-    public class IImageService
+    public interface IImageService
     {
+        Task uploadAsync(string imageName, Stream stream);
+        Task<List<ImageDetails>> GetImageListAsync(int hotelId);
+        Task<List<IFormFile>> UploadImagesAsync(List<IFormFile> files, int hotelid);
+        Task DeleteFileAsync(string fileName);
+        Task DeleteAllFileAsync(int hotelId);
     }
 }
