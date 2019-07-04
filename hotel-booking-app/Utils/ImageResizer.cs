12dbbd8ffcb1cec6a7b23b990e2bdbd556c1e85c
Gergely Kocsis <koger23@gmail.com>
2019-07-04 16:42:37 +0200

    feat(ImageResizer): init class with ResizeImage method

---
+++ hotel-booking-app/Utils/ImageResizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace HotelBookingApp.Utils
+{
+    public class ImageResizer
+    {
+        internal static Bitmap ResizeImage(Bitmap bitmap, int size1, int size2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
