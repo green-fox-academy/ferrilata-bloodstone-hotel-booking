12b2872d998ff6cb36bc1928d9bbe96ce21ad3b5
Gergely Kocsis <koger23@gmail.com>
2019-07-04 16:30:42 +0200

    feat(Hotel): add new properties
    realted to thumbnail

--- hotel-booking-app/Models/Hotel/Hotel.cs
+++ hotel-booking-app/Models/Hotel/Hotel.cs
@@ -9,13 +9,13 @@ namespace HotelBookingApp.Models.Hotel
         public string Description { get; set; }
         public int Price { get; set; }
         public int StarRating { get; set; }
-
         public Location Location { get; set; }
         public int LocationId { get; set; }
-
         public PropertyType PropertyType { get; set; }
         public int PropertyTypeId { get; set; }
-
         public IEnumerable<Room> Rooms { get; set; }
+        public bool Thumbnail { get; set; } = false;
+        public string ThumbnailUrl { get; set; }
     }
+}
 }
