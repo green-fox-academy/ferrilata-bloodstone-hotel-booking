dfbee6e38040164a7927bdf8435fb5cd719dde01
Gergely Kocsis <koger23@gmail.com>
2019-07-05 13:22:46 +0200

    feat(HotelViewModel): create view DTO

---
+++ hotel-booking-app/Models/Hotel/HotelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HotelBookingApp.Models.Hotel;
+using HotelBookingApp.Models.Image;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace HotelBookingApp.Pages
+{
+    public class HotelViewModel
+    {
+        public Hotel Hotel { get; set; }
+        public List<SelectListItem> PropertyTypes { get; set; }
+        public List<ImageDetails> ImageList { get; set; }
+    }
+}
