d7fb7e24bac3804d46c44a4a1ab432794073e818
Gergely Kocsis <koger23@gmail.com>
2019-07-05 14:07:49 +0200

    feat(HotelsController): implement HotelViewModel for images

--- hotel-booking-app/Controllers/HotelsController.cs
+++ hotel-booking-app/Controllers/HotelsController.cs
@@ -1,8 +1,11 @@
-﻿using HotelBookingApp.Exceptions;
+using HotelBookingApp.Exceptions;
 using HotelBookingApp.Models.Hotel;
+using HotelBookingApp.Pages;
 using HotelBookingApp.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace HotelBookingApp.Controllers
@@ -11,10 +14,12 @@ namespace HotelBookingApp.Controllers
     public class HotelsController : Controller
     {
         private readonly IHotelService hotelService;
+        private readonly IImageService imageService;
 
-        public HotelsController(IHotelService hotelService)
+        public HotelsController(IHotelService hotelService, IImageService imageService)
         {
             this.hotelService = hotelService;
+            this.imageService = imageService;
         }
 
         [HttpGet("/hotel/{id}")]
@@ -23,7 +28,13 @@ namespace HotelBookingApp.Controllers
             try
             {
                 var hotel = await hotelService.FindByIdAsync(id);
-                return View(hotel);
+                var images = await imageService.GetImageListAsync(id);
+                var model = new HotelViewModel
+                {
+                    Hotel = hotel,
+                    ImageList = images
+                };
+                return View(model);
             }
             catch (ItemNotFoundException ex)
             {
@@ -36,15 +47,22 @@ namespace HotelBookingApp.Controllers
         public async Task<IActionResult> Edit(int id)
         {
             var hotel = await hotelService.FindByIdAsync(id);
-            return View(hotel);
+            var images = await imageService.GetImageListAsync(id);
+            var model = new HotelViewModel
+            {
+                Hotel = hotel,
+                ImageList = images
+            };
+            return View(model);
         }
 
         [Authorize(Roles = "Admin, HotelManager")]
         [HttpPost("/hotel/edit/{id}")]
-        public async Task<IActionResult> Edit(int id, Hotel hotel)
+        public async Task<IActionResult> Edit(int id, Hotel hotel, List<IFormFile> imageList)
         {
             hotel.HotelId = id;
             await hotelService.Update(hotel);
+            await imageService.UploadImagesAsync(imageList, id);
             return RedirectToAction(nameof(Hotel), new { id } );
         }
     }
