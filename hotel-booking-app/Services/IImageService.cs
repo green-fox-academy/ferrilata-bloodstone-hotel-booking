652d2461f5718a356c742400f614eab83ba9018a
Gergely Kocsis <koger23@gmail.com>
2019-07-08 10:09:57 +0200

    fix(ImageService, IImageService): change protection to private
    UploadAsync() method used only in ImageService

--- hotel-booking-app/Services/IImageService.cs
+++ hotel-booking-app/Services/IImageService.cs
@@ -8,7 +8,6 @@ namespace HotelBookingApp.Services
 {
     public interface IImageService
     {
-        Task UploadAsync(string imageName, Stream stream);
         Task<List<ImageDetails>> GetImageListAsync(int hotelId);
         Task<List<IFormFile>> UploadImagesAsync(List<IFormFile> files, int hotelid);
         Task DeleteFileAsync(string fileName);
