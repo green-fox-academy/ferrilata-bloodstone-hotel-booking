c605d0308e5e73dc52f574dd8228765abe07ad9d
Krisz Magyar <krisz.magyar@gmail.com>
2019-07-08 15:00:18 +0200

    feat(RoomSController). init

---
+++ hotel-booking-app/Controllers/RoomsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingApp.Controllers
+{
+    [Route("hotel/{hotelId}/rooms")]
+    public class RoomsController : Controller
+    {
+        [HttpGet("{roomId}/reservation/new")]
+        public IActionResult NewReservation(int hotelId, int roomId)
+        {
+            return View();
+        }
+    }
+}
