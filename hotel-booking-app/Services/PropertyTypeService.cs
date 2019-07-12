d4199196e92a6f841fd93e6933d8ec4a08cde126
Gergely Kocsis <koger23@gmail.com>
2019-07-12 09:06:24 +0200

    refactor(PropertyTypeService): move translating to service

--- hotel-booking-app/Services/PropertyTypeService.cs
+++ hotel-booking-app/Services/PropertyTypeService.cs
@@ -1,6 +1,7 @@
 ﻿using HotelBookingApp.Data;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,18 @@ namespace HotelBookingApp.Services
     public class PropertyTypeService : IPropertyTypeService
     {
         private readonly ApplicationContext applicationContext;
+        private readonly IStringLocalizer<PropertyTypeService> localizer;
 
-        public PropertyTypeService(ApplicationContext applicationContext)
+        public PropertyTypeService(ApplicationContext applicationContext, IStringLocalizer<PropertyTypeService> localizer)
         {
             this.applicationContext = applicationContext;
+            this.localizer = localizer;
         }
 
         public async Task<IEnumerable<SelectListItem>> FindAll()
         {
             return await applicationContext.PropertyTypes
-                .Select(pt => new SelectListItem { Value = Convert.ToString(pt.PropertyTypeId), Text = pt.Type })
+                .Select(pt => new SelectListItem { Value = Convert.ToString(pt.PropertyTypeId), Text = localizer[pt.Type] })
                 .ToListAsync();
         }
     }
