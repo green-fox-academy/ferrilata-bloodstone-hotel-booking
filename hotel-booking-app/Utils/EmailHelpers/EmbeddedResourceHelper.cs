9d7a78e0e31360a4a328b5c6062644515c838e6b
Gergely Kocsis <koger23@gmail.com>
2019-07-18 12:54:52 +0200

    feat: add classes for Email Service

---
+++ hotel-booking-app/Utils/EmailHelpers/EmbeddedResourceHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace HotelBookingApp.Utils.EmailHelpers
+{
+    internal static class EmbeddedResourceHelper
+    {
+        internal static string GetResourceAsString(Assembly assembly, string path)
+        {
+            using (var stream = assembly.GetManifestResourceStream(path))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var result = reader.ReadToEnd();
+                    return result;
+                }
+            }
+        }
+    }
+}
