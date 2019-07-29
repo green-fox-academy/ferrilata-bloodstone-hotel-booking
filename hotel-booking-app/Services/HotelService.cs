9af9ec316bd1a507a5fd1300d625b80522934106
Gergely Kocsis <koger23@gmail.com>
2019-07-29 16:43:07 +0200

    feat(HotelServiceTest): add mocked automapper

--- hotel-booking-app/Services/HotelService.cs
+++ hotel-booking-app/Services/HotelService.cs
@@ -5,7 +5,6 @@ using HotelBookingApp.Models.HotelModels;
 using HotelBookingApp.Utils;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
