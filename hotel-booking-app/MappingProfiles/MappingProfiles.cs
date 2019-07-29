23a5171c8f6ee818f692b939b0f4f4e6cb137baf
Gergely Kocsis <koger23@gmail.com>
2019-07-29 15:41:33 +0200

    feat(ServiceCollectionExtensions.cs): create automapper profiles

---
+++ hotel-booking-app/MappingProfiles/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HotelBookingApp.Models.HotelModels;
+
+namespace HotelBookingApp.MappingProfiles
+{
+    public class MappingProfiles
+    {
+        public static MapperConfiguration GetAutoMapperProfiles()
+        {
+            return new MapperConfiguration(mc =>
+            {
+                mc.CreateMap<Hotel, ApiHotelDTO>()
+                    .ForMember(dest => dest.City, opts => opts.MapFrom(src => src.Location.City))
+                    .ForMember(dest => dest.Country, opts => opts.MapFrom(src => src.Location.Country))
+                    .ForMember(dest => dest.Address, opts => opts.MapFrom(src => src.Location.Address));
+            });
+        }
+    }
+}
