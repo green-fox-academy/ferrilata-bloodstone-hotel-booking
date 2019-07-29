c5255a7a5e9671fc598db5ec779095bf85b51ef4
Gergely Kocsis <koger23@gmail.com>
2019-07-29 13:57:13 +0200

    feat(ApiController): create new controller

---
+++ hotel-booking-app/Controllers/ApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelBookingApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingApp.Controllers
+{
+    [Route("[controller]/")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly IHotelService hotelService;
+
+        public ApiController(IHotelService hotelService)
+        {
+            this.hotelService = hotelService;
+        }
+
+        [HttpGet("hotels")]
+        public async Task<object> hotels(string city)
+        {
+            var paginatedHotels = await hotelService.FindWithQuery(new Utils.QueryParams { Search=city});
+            return paginatedHotels.CurrentPage;
+        }
+    }
+}
