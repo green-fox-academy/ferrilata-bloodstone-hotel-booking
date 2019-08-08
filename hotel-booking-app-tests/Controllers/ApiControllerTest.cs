81f33c2543433ece70602ad4fef3dc438dbcf074
Gergely Kocsis <koger23@gmail.com>
2019-08-08 17:35:03 +0200

    feat(ApiControllerTest): init

---
+++ hotel-booking-app-tests/Controllers/ApiControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HotelBookingApp.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBookingAppTests.Controllers
+{
+    class ApiControllerTest
+    {
+        private readonly Mock<IHotelService> hotelServiceMock;
+        private readonly Mock<IRoomService> roomServiceMock;
+        private readonly Mock<IAccountService> accountServiceMock;
+        private readonly Mock<IReservationService> reservationServiceMock;
+        private readonly Mock<IMapper> mapperMock;
+        private readonly Mock<IImageService> imageServiceMock;
+
+        public ApiControllerTest()
+        {
+            hotelServiceMock = new Mock<IHotelService>();
+            roomServiceMock = new Mock<IRoomService>();
+            accountServiceMock = new Mock<IAccountService>();
+            reservationServiceMock = new Mock<IReservationService>();
+            mapperMock = new Mock<IMapper>();
+            imageServiceMock = new Mock<IImageService>();
+        }
+    }
+}
