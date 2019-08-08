375e897b02154cb151e680ed1faa63835431a4fa
Kata <Kata@1743f.w.time4vps.cloud>
2019-08-08 15:43:25 +0200

    test(Controller): RoomController-Add bed

--- hotel-booking-app-tests/Controllers/RoomsControllerTests.cs
+++ hotel-booking-app-tests/Controllers/RoomsControllerTests.cs
@@ -39,5 +39,22 @@ namespace HotelBookingAppTests.Controllers
             Assert.Equal(nameof(HotelsController.Hotel), redirectResult.ActionName);
             roomServiceMock.Verify(s => s.AddRoom(hotelId, room), Times.Once);
         }
+
+        [Fact]
+        public async Task AddBed_WhenValid_ShouldCallServiceAndRedirect()
+        {
+            var controller = new RoomsController(bedServiceMock.Object, roomServiceMock.Object);
+            var hotelId = 1;
+            var bedViewModel = new BedViewModel();
+
+            var result = await controller.AddBed(hotelId, bedViewModel);
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            var resultHotelId = (int)redirectResult.RouteValues["id"];
+
+            Assert.Equal("Hotels", redirectResult.ControllerName);
+            Assert.Equal(nameof(HotelsController.Hotel), redirectResult.ActionName);
+            Assert.Equal(hotelId, resultHotelId);
+            bedServiceMock.Verify(s => s.AddBed(bedViewModel), Times.Once);
+        }
     }
 }
