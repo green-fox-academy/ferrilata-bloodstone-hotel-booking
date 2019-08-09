6f9632ff3b419840a0feedf4007045d12a188ab5
Gergely Kocsis <koger23@gmail.com>
2019-08-09 12:37:51 +0200

    feat(ApiControllerTest): add new method

--- hotel-booking-app-tests/Controllers/ApiControllerTest.cs
+++ hotel-booking-app-tests/Controllers/ApiControllerTest.cs
@@ -1,20 +1,19 @@
 ﻿using AutoMapper;
-using HotelBookingApp.Services;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
-using HotelBookingAppTests.TestUtils;
-using System.Threading.Tasks;
 using HotelBookingApp.Controllers;
-using HotelBookingApp.Utils;
-using Microsoft.AspNetCore.Mvc;
-using System.Web.Mvc;
-using System.Net;
-using HotelBookingApp.Models.HotelModels;
+using HotelBookingApp.Data;
 using HotelBookingApp.Models.API;
+using HotelBookingApp.Models.HotelModels;
+using HotelBookingApp.Services;
+using HotelBookingApp.Utils;
+using HotelBookingAppTests.TestUtils;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace HotelBookingAppTests.Controllers
 {
@@ -26,6 +25,7 @@ namespace HotelBookingAppTests.Controllers
         private readonly Mock<IReservationService> reservationServiceMock;
         private readonly Mock<IMapper> mapperMock;
         private readonly Mock<IImageService> imageServiceMock;
+        private readonly DbContextOptions<ApplicationContext> options = TestDbOptions.Get();
 
         public ApiControllerTest()
         {
@@ -38,14 +38,56 @@ namespace HotelBookingAppTests.Controllers
         }
 
         [Fact]
-        public async Task FetchHotels_WhenValid_ShouldCallServiceAndResponseOkWithHotel()
+        public async Task FetchHotels_WhithInvalidCurrentPage_ShouldCallServiceAndResponseBadRequest()
+        {
+
+            // Arrange
+            var hotel = new Hotel { Name = "Test hotel" };
+            var paginatedList = new PaginatedList<Hotel>();
+            using (var context = new ApplicationContext(options))
+            {
+                int currentPage = 1;
+                int pageSize = 1;
+                paginatedList = await PaginatedList<Hotel>
+                       .CreateAsync(context.Hotels.Take(12), currentPage, pageSize);
+            }
+            paginatedList.Add(hotel);
+            var hotelDTOlist = new List<HotelDTO>();
+            int curPage = 99;
+            var hotelsDto = new HotelsDTO
+            {
+                PageCount = 1,
+                CurrentPage = curPage,
+                Hotels = hotelDTOlist
+            };
+            var q = new QueryParams();
+            q.CurrentPage = curPage;
+            q.Search = "";
+
+            hotelServiceMock.Setup(h => h.FindWithQuery(q))
+                .ReturnsAsync(paginatedList);
+
+            var controller = new ApiController(hotelServiceMock.Object, roomServiceMock.Object, accountServiceMock.Object, reservationServiceMock.Object, mapperMock.Object, imageServiceMock.Object);
+
+            // Act
+            var result = await controller.Hotels(q);
+            var badRequestResult = result as ObjectResult;
+
+            // Assert
+            Assert.NotNull(badRequestResult);
+            Assert.True(badRequestResult is BadRequestObjectResult);
+            Assert.Equal(StatusCodes.Status400BadRequest, badRequestResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task FetchHotels_WithValidQueryParams_ShouldCallServiceAndResponseOkWithHotel()
         {
             // Arrange
-            var hotel = new Hotel { Name = "Test hotel"};
+            var hotel = new Hotel { Name = "Test hotel" };
             var hotels = new PaginatedList<Hotel>();
             hotels.Add(hotel);
             var hotelDTOlist = new List<HotelDTO>();
-            hotelDTOlist.Add(new HotelDTO { Name = "Test hotel" });
+            hotelDTOlist.Add(new HotelDTO { Name = "Test hotel"});
             var hotelsDto = new HotelsDTO
             {
                 PageCount = 1,
